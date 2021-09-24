f59444842a8cb38060428fa5bda87e21428fbb1f
jwright90 <jason.wright90@gmail.com>
2021-09-24 19:13:15 +0100

    finish custom material ui theme

--- frontend/src/components/ui/theme/theme.js
+++ frontend/src/components/ui/theme/theme.js
@@ -5,7 +5,8 @@ const darkGreen = "#708670"
 const tan = "#FECEA8"
 const lightRed = "#FF847C"
 const red = "#E84A5F"
-const offBlack = "2A363B"
+const offBlack = "#2A363B"
+const grey = "#747474"
 
 const theme = createTheme({
   palette: {
@@ -27,16 +28,50 @@ const theme = createTheme({
       fontSize: "4.5rem",
       fontFamily: "Philosopher",
       fontStyle: "italic",
-      fontWeight: 700
+      fontWeight: 700,
+      color: green
+    },
+
+    h2: {
+      fontSize: "3rem",
+      fontFamily: "Montserrat",
+      fontWeight: 500,
+      color: "#fff"
+    },
+
+    h3: {
+      fontSize: "2rem",
+      fontFamily: "Montserrat",
+      fontWeight: 300,
+      color: green
+    },
+
+    h4: {
+      fontSize: "3rem",
+      fontFamily: "Philosopher",
+      fontStyle: "italic",
+      fontWeight: 700,
+      color: "#fff"
+    },
+
+    h5: {
+      fontFamily: "Philosopher",
+      fontSize: "2rem",
+      fontWeight: 700,
+      fontStyle: "italic",
+      color: "#fff"
+    },
+
+    body1: {
+      fontFamily: "Montserrat",
+      fontSize: "1.5rem",
+      color: grey
     },
 
     purpleButton: {
       fontFamily: ""
     },
 
-    body1: {
-
-    },
   },
   overides: {}
 });
